b0b1a78a7e2af786323e975fab9493568c6f03ab
Matt Free <mjfree34@gmail.com>
2024-12-18 22:56:02 -0500

    Day 5 Part 2

--- day05/day05.py
+++ day05/day05.py
@@ -37,7 +37,38 @@ def solve_part1(before_rules, updates):
     return sorted_middle_pages_sum
 
 def solve_part2(before_rules, after_rules, updates):
-    pass
+    sorted_middle_pages_sum = 0
+
+    for update in updates:
+        is_sorted = True
+
+        for i, x in enumerate(update):
+            for j, y in enumerate(update):
+                if i < j and y in before_rules[x]:
+                    is_sorted = False
+                    break
+            if not is_sorted:
+                break
+        
+        if not is_sorted:
+            sorted_update = []
+            Q = deque([])
+            D = {v: len(before_rules[v] & set(update)) for v in update}
+            for v in update:
+                if D[v] == 0:
+                    Q.append(v)
+            while Q:
+                v = Q.popleft()
+                sorted_update.append(v)
+                for u in after_rules[v]:
+                    if u in D:
+                        D[u] -= 1
+                        if D[u] == 0:
+                            Q.append(u)
+            sorted_middle_pages_sum += sorted_update[len(sorted_update) // 2]
+    
+    return sorted_middle_pages_sum
+
 
 if __name__ == "__main__":
     file_path = '/Users/mattfree/Desktop/AdventOfCode/day05/input.txt'
@@ -45,3 +76,6 @@ if __name__ == "__main__":
 
     part1_result = solve_part1(before_rules, updates)
     print(f"Part 1: {part1_result}")
+
+    part2_result = solve_part2(before_rules, after_rules, updates)
+    print(f"Part 2: {part2_result}")
