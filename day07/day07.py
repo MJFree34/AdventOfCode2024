b80ff3f4d0ede8217f3878d05376b5150ebe6772
Matt Free <mjfree34@gmail.com>
2024-12-20 12:08:35 -0500

    Day 7 Part 2

--- day07/day07.py
+++ day07/day07.py
@@ -1,7 +1,8 @@
 class Operation:
-    def __init__(self, test_value: int, numbers: list[int]):
+    def __init__(self, test_value: int, numbers: list[int], concatenate: bool = False):
         self.test_value = test_value
         self.numbers = numbers
+        self.concatenate = concatenate
         self.possible = self.test_possible(numbers[0], 1)
 
     def test_possible(self, curr_total: int, start_i: int) -> bool:
@@ -9,10 +10,10 @@ class Operation:
             return curr_total == self.test_value
         mult_total = self.numbers[start_i] * curr_total
         add_total = self.numbers[start_i] + curr_total
+        if self.concatenate:
+            concatenate_total = int(str(curr_total) + str(self.numbers[start_i]))
+            return self.test_possible(mult_total, start_i + 1) or self.test_possible(add_total, start_i + 1) or self.test_possible(concatenate_total, start_i + 1)
         return self.test_possible(mult_total, start_i + 1) or self.test_possible(add_total, start_i + 1)
-
-    def __str__(self):
-        return f"Test Value: {self.test_value}, Numbers: {self.numbers}, Is Possible: {self.possible}"
 
 def read_input(file_path: str) -> list[str]:
     with open(file_path, 'r') as file:
@@ -32,7 +33,14 @@ def solve_part1(data: list[list[str]]) -> int:
     return possible_operations_sum
 
 def solve_part2(data):
-    pass
+    possible_operations_sum = 0
+    for line in data:
+        test_value = int(line[0])
+        numbers = [int(num) for num in line[1].split()]
+        operation = Operation(test_value, numbers, concatenate=True)
+        if operation.possible:
+            possible_operations_sum += operation.test_value
+    return possible_operations_sum
 
 if __name__ == "__main__":
     input_data = read_input('/Users/mattfree/Desktop/AdventOfCode/day07/input.txt')
