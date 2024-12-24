23c638efb6c29490cd2a1beb9b8ea037682976d8
Matt Free <mjfree34@gmail.com>
2024-12-23 22:52:49 -0500

    Day 23 Part 1

---
+++ day23/day23.py
@@ -0,0 +1,43 @@
+graph: dict[str, set[str]] = {}
+
+def read_input(file_path: str) -> list[str]:
+    with open(file_path, 'r') as file:
+        return file.read().strip().split('\n')
+
+def process_input(input_data: list[str]):
+    for line in input_data:
+        u, v = line.split('-')
+        if u not in graph:
+            graph[u] = set()
+        graph[u].add(v)
+        if v not in graph:
+            graph[v] = set()
+        graph[v].add(u)
+    print(graph)
+
+def solve_part1() -> int:
+    three_sets_count = 0
+    for a in graph:
+        connected_nodes = list(graph[a])
+        for i in range(len(connected_nodes) - 1):
+            for j in range(i + 1, len(connected_nodes)):
+                b = connected_nodes[i]
+                c = connected_nodes[j]
+                if c in graph[b] and (a.startswith('t') or b.startswith('t') or c.startswith('t')):
+                    three_sets_count += 1
+                    print(a, b, c)
+        
+    return three_sets_count // 3
+
+def solve_part2():
+    pass
+
+if __name__ == "__main__":
+    input_data = read_input('/Users/mattfree/Desktop/AdventOfCode/day23/input.txt')
+    process_input(input_data)
+
+    result_part1 = solve_part1()
+    print("Part 1:", result_part1)
+
+    result_part2 = solve_part2()
+    print("Part 2:", result_part2)
